fb14c3a3a80510a4b08f660e12f1f64a57941ae2
KGCM28 <78344641+KGCM28@users.noreply.github.com>
2021-03-22 18:25:51 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,298 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput} from 'react-native';
+
+
+
+export default function App() {
+  return (
+
+//  1   
+    <ScrollView>
+    <View style={styles.container}>
+
+    <TextInput style = {{
+      height: 100,
+       borderColor:'#87ceeb', 
+       borderWidth: 1}} 
+       placeholder = " " value =" "/>
+
+    <Text style ={{
+      height: 10, 
+      borderColor: '#87ceeb',
+       borderWidth: 10}}
+        > CatsApp </Text>
+      
+      <Text style ={{
+        height: 10, borderColor: 
+        '#87ceeb', borderWidth: 15}}
+         > </Text>
+      
+      <Image style = {{width: 550, height: 500}}source = {{
+        uri: "https://media.tenor.com/images/9d16e0e7295fdf8cf7d6f1978365c4e5/tenor.gif"}}></Image>
+      
+
+      
+    </View>
+    <Text style ={{height: 15, 
+      borderColor: 'gray', 
+      borderWidth: 8}} 
+      > What is the Cat name? </Text>
+
+      <TextInput style = {{
+        height: 15, 
+        borderColor:'#87ceeb', 
+        borderWidth: 1}} 
+        placeholder = "What is the Cat name?" 
+        value ="Enter a Name."/>
+
+{/* 2 */}
+
+<View style={styles.container}>
+
+<TextInput style = {{
+  height: 100,
+   borderColor:'#87ceeb', 
+   borderWidth: 1}} 
+   placeholder = " " value =" "/>
+
+<Text style ={{
+  height: 10, 
+  borderColor: '#87ceeb',
+   borderWidth: 10}}
+    > Who is this brown cat? </Text>
+  
+  <Text style ={{
+    height: 10, borderColor: 
+    '#87ceeb', borderWidth: 15}}
+     > </Text>
+  
+  <Image style = {{width: 550, height: 500}}source = {{
+    uri: "https://media.tenor.com/images/888ebb411061bdaf7a949f4c5b1e818c/tenor.gif"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+  borderColor: 'gray', 
+  borderWidth: 8}} 
+  > What is the Cat name? </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "What is the Cat name?" 
+    value ="Enter a Name."/>
+
+{/* 3 */}
+
+<View style={styles.container}>
+
+<TextInput style = {{
+  height: 100,
+   borderColor:'#87ceeb', 
+   borderWidth: 1}} 
+   placeholder = " " value =" "/>
+
+<Text style ={{
+  height: 10, 
+  borderColor: '#87ceeb',
+   borderWidth: 10}}
+    > aren't they cute!? </Text>
+  
+  <Text style ={{
+    height: 10, borderColor: 
+    '#87ceeb', borderWidth: 15}}
+     > </Text>
+  
+  <Image style = {{width: 450, height: 500}}source = {{
+    uri: "https://media.tenor.com/images/5aa9137a9e2c0b85432076f02513ad8f/tenor.gif"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+  borderColor: 'gray', 
+  borderWidth: 8}} 
+  > are they couples!? </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "are they couples!? " 
+    value =" "/>
+
+{/* 4 */}
+
+<View style={styles.container}>
+
+<TextInput style = {{
+  height: 100,
+   borderColor:'#87ceeb', 
+   borderWidth: 1}} 
+   placeholder = " " value =" "/>
+
+<Text style ={{
+  height: 10, 
+  borderColor: '#87ceeb',
+   borderWidth: 10}}
+    > aweeee they are so adorable! </Text>
+  
+  <Text style ={{
+    height: 10, borderColor: 
+    '#87ceeb', borderWidth: 15}}
+     > </Text>
+  
+  <Image style = {{width: 450, height: 500}}source = {{
+    uri: "https://media.tenor.com/images/1941188883f8c7211274f353ea8308cd/tenor.gif"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+  borderColor: 'gray', 
+  borderWidth: 8}} 
+  > hahaha so funny! </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "hahaha so funny!" 
+    value =" "/>
+
+{/* 5 */}
+
+<View style={styles.container}>
+
+<TextInput style = {{
+  height: 100,
+   borderColor:'#87ceeb', 
+   borderWidth: 1}} 
+   placeholder = " " value =" "/>
+
+<Text style ={{
+  height: 10, 
+  borderColor: '#87ceeb',
+   borderWidth: 10}}
+    > aweeee they are so so so so adorable! </Text>
+  
+  <Text style ={{
+    height: 10, borderColor: 
+    '#87ceeb', borderWidth: 15}}
+     > </Text>
+  
+  <Image style = {{width: 450, height: 500}}source = {{
+    uri: "https://media.tenor.com/images/aaf9a36095449a7e70ae457f2aa6273e/tenor.gif"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+  borderColor: 'gray', 
+  borderWidth: 8}} 
+  > hahaha so cuttiesss ! </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "ahaha so cuttiesss !" 
+    value =" "/>
+
+
+{/* 6 */}
+
+<View style={styles.container}>
+
+<TextInput style = {{
+  height: 100,
+   borderColor:'#87ceeb', 
+   borderWidth: 1}} 
+   placeholder = " " value =" "/>
+
+<Text style ={{
+  height: 10, 
+  borderColor: '#87ceeb',
+   borderWidth: 10}}
+    > what a cute couples </Text>
+  
+  <Text style ={{
+    height: 10, borderColor: 
+    '#87ceeb', borderWidth: 15}}
+     > </Text>
+  
+  <Image style = {{width: 450, height: 500}}source = {{
+    uri: "https://media.tenor.com/images/064d8f67637ad2e0ec3797b2dddf639d/tenor.gif"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+  borderColor: 'gray', 
+  borderWidth: 8}} 
+  > i love them ! </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "i love them !" 
+    value =" "/>
+
+{/* 7 */}
+
+<View style={styles.container}>
+
+<TextInput style = {{
+  height: 100,
+   borderColor:'#87ceeb', 
+   borderWidth: 1}} 
+   placeholder = " " value =" "/>
+
+<Text style ={{
+  height: 10, 
+  borderColor: '#87ceeb',
+   borderWidth: 10}}
+    > aweeee last page :( </Text>
+  
+  <Text style ={{
+    height: 10, borderColor: 
+    '#87ceeb', borderWidth: 15}}
+     > </Text>
+  
+  <Image style = {{width: 450, height: 500}}source = {{
+    uri: "https://media.tenor.com/images/d31d4f2e01df4c51721f48ca4c84df27/tenor.gif"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+  borderColor: 'gray', 
+  borderWidth: 8}} 
+  > hope you like it! </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "hope you like it!" 
+    value =" "/>
+
+
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#87ceeb',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    
+  },
+});
